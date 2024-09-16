ff02964b5d8d9f458ba07c985936880a28fffce2
goochul <112638535+gooch123@users.noreply.github.com>
2024-09-16 10:29:55 +0900

    RT 3일 수정

--- src/main/java/towssome/server/jwt/JwtStatic.java
+++ src/main/java/towssome/server/jwt/JwtStatic.java
@@ -3,7 +3,7 @@ package towssome.server.jwt;
 public class JwtStatic {
 
     public static final Long ACCESS_EXPIRE_MS =1800000L;
-    public static final Long REFRESH_EXPIRE_MS =86400000L;
+    public static final Long REFRESH_EXPIRE_MS =259200000L;
 
 
 }
