456bb7b030e866ac48a87843676967cb661ec671
goochul <gooch123@naver.com>
2025-01-07 14:01:45 +0900

    refactor(clendar): 캘린더로 모든 태그 찾기 메소드 추가

--- src/main/java/towssome/server/repository/CalendarPersonalScheduleRepository.java
+++ src/main/java/towssome/server/repository/CalendarPersonalScheduleRepository.java
@@ -1,7 +1,13 @@
 package towssome.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import towssome.server.entity.Calendar;
 import towssome.server.entity.CalendarPersonalSchedule;
 
+import java.util.List;
+
 public interface CalendarPersonalScheduleRepository extends JpaRepository<CalendarPersonalSchedule,Long> {
+
+    List<CalendarPersonalSchedule> findAllByCalendar(Calendar calendar);
+
 }
